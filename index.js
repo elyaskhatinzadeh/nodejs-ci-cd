dc65a992b3f7cc31ce02ef64abd2402c5f44aef7
Elyas Khatinzadeh <amin72e@gmail.com>
2023-12-30 02:48:30 +0330

    ci/cd

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const port = 3000;
 
 // Define a simple route
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, welcome to the Node.js API!' });
+    res.json({
+        data:  true,
+        message: 'Hello, welcome to the Node.js API!'
+    });
 });
 
 app.get('/home', (req, res) => {
